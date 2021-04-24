a5b9dde62441fe543560b94b55feac679240909c
Thibault Garçon <thibault.garcon@pole-emploi.fr>
2021-04-24 13:12:06 +0200

    CHORE : maj version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="pygitscrum",
-    version="0.2.0",
+    version="0.3.0",
     description="pygitscrum",
     long_description="The complete description/installation/use/FAQ is available at : https://github.com/thib1984/pygitscrum#readme",
     url="https://github.com/thib1984/pygitscrum",
